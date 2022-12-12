246afff074ee1a8a67a7389a1c95da500d97de0f
my-roki <anthonychangrok@gmail.com>
2022-12-12 18:49:39 +0900

    chore: 톰캣으로 웹 애플리케이션 실행

--- src/main/java/com/myroki/todolist/TodoListApplication.java
+++ src/main/java/com/myroki/todolist/TodoListApplication.java
@@ -2,14 +2,20 @@ package com.myroki.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class TodoListApplication {
+public class TodoListApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(TodoListApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(TodoListApplication.class);
+    }
 }
