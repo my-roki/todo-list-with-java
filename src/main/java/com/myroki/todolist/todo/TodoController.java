100717ec3c8f2f6f3357b4a422a158a3a8318b9b
my-roki <anthonychangrok@gmail.com>
2022-12-12 15:22:01 +0900

    Feat: Add todolist validation

--- src/main/java/com/myroki/todolist/todo/TodoController.java
+++ src/main/java/com/myroki/todolist/todo/TodoController.java
@@ -3,18 +3,24 @@ package com.myroki.todolist.todo;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
 import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@Validated
+@CrossOrigin(origins = {" https://todobackend.com/", "https://todobackend.com/"},
+        methods = {RequestMethod.PATCH, RequestMethod.POST, RequestMethod.GET, RequestMethod.DELETE, RequestMethod.OPTIONS})
 public class TodoController {
     private final TodoService todoService;
     private final TodoMapper mapper;
 
     @PostMapping
-    public ResponseEntity postList(@RequestBody TodoDto.Post requestBody) {
+    public ResponseEntity postList(@RequestBody @Valid TodoDto.Post requestBody) {
         Todo todo = mapper.todoPostDtoToTodo(requestBody);
         Todo serviceTodo = todoService.createTodo(todo);
         TodoDto.Response response = mapper.todoToTodoResponseDto(serviceTodo);
@@ -31,7 +37,7 @@ public class TodoController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity getTodoList(@PathVariable("id") long todolistId) {
+    public ResponseEntity getTodoList(@PathVariable("id") @Positive long todolistId) {
         Todo todo = todoService.findTodoById(todolistId);
         TodoDto.Response response = mapper.todoToTodoResponseDto(todo);
 
@@ -39,8 +45,8 @@ public class TodoController {
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity patchTodoList(@PathVariable("id") long todolistId,
-                                        @RequestBody TodoDto.Patch requestBody) {
+    public ResponseEntity patchTodoList(@PathVariable("id") @Positive long todolistId,
+                                        @RequestBody @Valid TodoDto.Patch requestBody) {
         Todo todo = mapper.todoPatchDtoToTodo(requestBody);
         todo.setId(todolistId);
         Todo updateTodo = todoService.updateTodo(todo);
@@ -57,7 +63,7 @@ public class TodoController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity deleteTodoList(@PathVariable("id") long todolistId) {
+    public ResponseEntity deleteTodoList(@PathVariable("id") @Positive long todolistId) {
         todoService.deleteTodoById(todolistId);
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
--- src/main/java/com/myroki/todolist/todo/TodoDto.java
+++ src/main/java/com/myroki/todolist/todo/TodoDto.java
@@ -3,13 +3,20 @@ package com.myroki.todolist.todo;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 public class TodoDto {
     @Getter
     public static class Post {
+        @NotEmpty
         private String title;
 
-        @JsonProperty("todo_order")
+        @JsonProperty("order")
+        @NotNull
+        @Range(min = 1, max = 4)
         private Integer todoOrder;
     }
 
@@ -17,8 +24,11 @@ public class TodoDto {
     public static class Patch {
         private String title;
 
-        @JsonProperty("todo_order")
+        @JsonProperty("order")
+        @Range(min = 1, max = 4)
         private Integer todoOrder;
+
+        // TODO true / false 외에 어떠한 문자도 받을 수 없게 검증하는 방법은 없을까?
         private boolean completed;
     }
 
@@ -28,7 +38,7 @@ public class TodoDto {
         private long id;
         private String title;
 
-        @JsonProperty("todo_order")
+        @JsonProperty("order")
         private Integer todoOrder;
         private boolean completed;
     }
